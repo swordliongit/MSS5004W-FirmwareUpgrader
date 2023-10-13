ec548480ada17e1a90dac0810795fe806b488761
swordliongit <kilicarslan.business@gmail.com>
2023-10-13 16:29:27 +0300

    Overhaul to configs and further error handling

--- crawler.py
+++ crawler.py
@@ -1,5 +1,3 @@
-
-
 import os
 import subprocess
 import sys
@@ -15,26 +13,37 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium import webdriver
 from selenium.webdriver.common.alert import Alert
 
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+
+from configparser import ConfigParser
+
+
 DEBUG = True
 TIMEOUT = 20
-main_dir = os.path.dirname(os.path.abspath(__file__))
-IMAGE_LOCATION = os.path.join(main_dir, 'firmware')
 CURRENT_MAC = ""
+current_dir = os.path.dirname(__file__)
+IMAGE_LOCATION = os.path.join(current_dir, "firmware")
+mac_list = []
+
+config = ConfigParser()
+config.read(os.path.join(current_dir, "config.ini"))
 
 
 def login(Console, driver):
-    url = "http://192.168.1.1/cgi-bin/luci"
-    username = "R3000admin"
-    password = "admin"
-    
+    url = config.get("luci", "url")
+    username = config.get("luci", "username")
+    password = config.get("luci", "password")
+    if DEBUG:
+        print(IMAGE_LOCATION, url, username, password)
+
     try:
-        
         driver.get(url)  # open the url
         driver.find_element(By.ID, "username").send_keys(username)
         driver.find_element(By.NAME, "password").send_keys(password)
         driver.find_element(By.ID, "login_in").click()  # login button
         sleep(0.5)
-        return driver
+        return True
     except:
         WriteToConsole(Console, "<< Login failed! >>\n")
 
@@ -43,17 +52,49 @@ def crawl(Console, driver):
     global CURRENT_MAC
     global IMAGE_LOCATION
     try:
-        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.LINK_TEXT, "LAN Information"))).click()
+        WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.LINK_TEXT, "LAN Information"))
+        ).click()
         sleep(3)
-        CURRENT_MAC = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "#ra0-ifc-mac"))).text
+        CURRENT_MAC = (
+            WebDriverWait(driver, 10)
+            .until(EC.presence_of_element_located((By.CSS_SELECTOR, "#ra0-ifc-mac")))
+            .text
+        )
+        if CURRENT_MAC in mac_list:
+            raise Exception("Upgrade aborted: Duplicate modem found!")
+        else:
+            mac_list.append(CURRENT_MAC)
         WriteToConsole(Console, f"Login: {CURRENT_MAC}\n")
-        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.LINK_TEXT, "System"))).click()
-        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.LINK_TEXT, "Backup / Flash Firmware"))).click()   
-        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "#keep"))).click()
-        image_button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "#image")))
+        WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.LINK_TEXT, "System"))
+        ).click()
+        WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.LINK_TEXT, "Backup / Flash Firmware"))
+        ).click()
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "#keep"))
+        ).click()
+        image_button = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "#image"))
+        )
         image_button.send_keys(IMAGE_LOCATION)
-        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "#maincontent > div > div:nth-child(5) > div > div > form > div:nth-child(2) > div > div > input.cbi-button.cbi-input-apply"))).click()
-        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "#maincontent > div > div:nth-child(2) > div:nth-child(3) > form:nth-child(2) > input.cbi-button.cbi-button-apply"))).click()
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located(
+                (
+                    By.CSS_SELECTOR,
+                    "#maincontent > div > div:nth-child(5) > div > div > form > div:nth-child(2) > div > div > input.cbi-button.cbi-input-apply",
+                )
+            )
+        ).click()
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located(
+                (
+                    By.CSS_SELECTOR,
+                    "#maincontent > div > div:nth-child(2) > div:nth-child(3) > form:nth-child(2) > input.cbi-button.cbi-button-apply",
+                )
+            )
+        ).click()
         # driver.close()
     except Exception as e:
         return False, e.__class__.__name__, str(e)
@@ -72,8 +113,10 @@ def logged_out(Console, driver):
 
 def get_device_mac():
     try:
-        arp_output = subprocess.check_output("arp -a", shell=True, universal_newlines=True)
-        lines = arp_output.strip().split('\n')
+        arp_output = subprocess.check_output(
+            "arp -a", shell=True, universal_newlines=True
+        )
+        lines = arp_output.strip().split("\n")
         for line in lines:
             parts = line.strip().split()
             if len(parts) == 3:
@@ -86,12 +129,13 @@ def get_device_mac():
 
 
 def WriteToConsole(Console, text):
-    Console.configure(state='normal')
+    Console.configure(state="normal")
     Console.insert(customtkinter.END, text)
-    Console.configure(state='disabled')
+    Console.configure(state="disabled")
+    Console.see(customtkinter.END)  # Scroll to the end
 
 
-def initiate(Console, modem_count):
+def initiate(Console, driver, button, modem_count):
     """_summary_
 
     Args:
@@ -99,35 +143,48 @@ def initiate(Console, modem_count):
         entry_1 (_type_): _description_
     """
     global CURRENT_MAC
+    global mac_list
     crawl_successful = False
     upgraded_count = 0
-    
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument("--headless")  # silent browser
-    driver = webdriver.Chrome(options=chrome_options)
+
+    WriteToConsole(Console, f"\n\nStarting...\n\n")
+
+    # chrome_options = webdriver.ChromeOptions()
+    # chrome_options.add_argument("--headless")  # silent browser
+    # driver = webdriver.Chrome(options=chrome_options)
     # driver = webdriver.Chrome()
-    
+
     WriteToConsole(Console, f"\n\nModem Count: {modem_count}\n\n")
-    
+
     for _ in range(modem_count):
-        if get_device_mac() is not None:
-            login(Console, driver)
+        if login(Console, driver):
             if not logged_out(Console, driver):
                 crawl_successful, excpt, excpt_msg = crawl(Console, driver)
                 if crawl_successful:
-                    WriteToConsole(Console, f"Firmware Upgrade Successful for: {CURRENT_MAC}\n")
+                    WriteToConsole(
+                        Console, f"Firmware Upgrade Successful for: {CURRENT_MAC}\n"
+                    )
                     upgraded_count += 1
                     if upgraded_count == modem_count:
                         break
                 else:
-                    WriteToConsole(Console, f"<< Firmware Upgrade Failed for: {CURRENT_MAC} >>\n")
+                    WriteToConsole(
+                        Console, f"<< Firmware Upgrade Failed for: {CURRENT_MAC} >>\n"
+                    )
                     WriteToConsole(Console, f"Error type: {excpt}\n")
                     WriteToConsole(Console, f"Error msg: {excpt_msg}\n")
+                    upgraded_count += 1
+                    if upgraded_count == modem_count:
+                        break
                     continue
         else:
             WriteToConsole(Console, "No modems found!\n\n")
             break
-        WriteToConsole(Console, "Waiting for the next modem...\n\n")
+        WriteToConsole(Console, "Waiting for the next modem input...\n\n")
         sleep(70)
-    WriteToConsole(Console, f"\n\n******Upgraded Modem Count: {upgraded_count}******\n\n")
+    WriteToConsole(
+        Console, f"\n\n******Upgraded Modem Count: {upgraded_count}******\n\n"
+    )
+    button.configure(state="normal")
+    mac_list.clear()
     driver.close()
